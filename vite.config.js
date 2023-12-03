c0ea7f423a0bd16c263935b176461fca08349e69
Ariano2700 <kawuai123loco123@gmail.com>
2023-12-03 12:34:23 -0500

    commit n3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
   export default defineConfig({
     plugins: [react()],
-    base:'https://ariano2700.github.io/GIF-Proyect.git'
+    base:'https://ariano2700.github.io/GIF-Proyect/'
   })
